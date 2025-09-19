a48219c38c6eb8fde1c14778b2b9ef962ce8180b
Fred Meza <freddy.meza.alfaro@gmail.com>
2025-09-18 21:23:05 -0300

    Modulo fma_lvgl para LILYGO 135x240 driver st7789

--- fma_components/fma_lvgl/include/fma_lvgl_lilygo.h
+++ fma_components/fma_lvgl/include/fma_lvgl_lilygo.h
@@ -1,12 +1,20 @@
 #ifndef FMA_LVGL_LILYGO_
 #define FMA_LVGL_LILYGO_
 
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_timer.h"
+#include "esp_log.h"
 
+#include "ui.h"
 #include "lvgl.h"
 
-extern lv_obj_t * display_subtitle;
-extern lv_obj_t * display_command;
+void fma_lvgl_init();
 
-void lvgl_init(char * title);
+// Solamente se declara static si va a ser usada dentro del mismo archivo .c
+void fma_lvgl_task(void *pv);
 
 #endif /* FMA_LVGL_LILYGO_ */
